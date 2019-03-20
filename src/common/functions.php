f689f4efb6129713de697d428f921fb2a72e6e30
mr.wind <zouxiuhui@vchangyi.com>
2019-03-20 16:40:33 +0800

    初始化composer files

---
+++ src/common/functions.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: asus
+ * Date: 2019/3/19
+ * Time: 20:45
+ */
+function hello_system()
+{
+    echo __FUNCTION__ . "@" . __FILE__ . PHP_EOL;
+}
---
+++ test.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: asus
+ * Date: 2019/3/19
+ * Time: 20:48
+ */
+require 'vendor/autoload.php';
+hello_system();
